2b4aad1b9e5bb4f7c714fa22f587a9ee1d009131
Enrique Pérez Olivares <enriquejpo@ext.zara.com>
2020-07-13 14:14:12 +0200

    Initial commit

---
+++ src/main/java/es/soprasteria/formacion/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package es.soprasteria.formacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.mongodb.EnableMongoConfigServer;
+
+@EnableMongoConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+}
